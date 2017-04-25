68ad54fc166e36f40eac1e220ae27f50c6b4dc11
Ichigo Mayo <IchigoMayo@mayodon.club>
2017-04-26 03:09:26 +0900

    cron: load rbenv

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,6 @@
+# coding: utf-8
 set :output, 'log/cron.log'
+job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
 every 8.hours, at: '0:12' do
   rake 'mastodon:daily mastodon:media:remove_remote'
