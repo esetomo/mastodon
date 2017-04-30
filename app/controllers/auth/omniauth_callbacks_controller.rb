1a48052948f373fb3728eb705ebdfb6dd6780db0
Ichigo Mayo <IchigoMayo@mayodon.club>
2017-04-30 19:20:34 +0900

    「連携先アカウントでログイン」機能を削除
    特に利便性に寄与せず、一方で連携先アカウントが乗っ取られたらログインできてしまうので削除しました。

--- app/controllers/auth/omniauth_callbacks_controller.rb
+++ app/controllers/auth/omniauth_callbacks_controller.rb
@@ -2,19 +2,10 @@
 
 class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def mastodon
-    account = FollowRemoteAccountService.new.call(request.env['omniauth.auth']['uid'])
+    return redirect_to :about unless current_user
     
-    if user_signed_in?
-      RemoteAccount.create(user_id: current_user.id, account_id: account.id)
-      redirect_to settings_remote_accounts_url
-    else
-      remote = account.remote_accounts.first
-      if remote
-        sign_in_and_redirect remote.user, :event => :authentication
-      else
-        # TODO error message
-        redirect_to new_user_session_url
-      end
-    end
+    account = FollowRemoteAccountService.new.call(request.env['omniauth.auth']['uid'])
+    RemoteAccount.create(user_id: current_user.id, account_id: account.id)
+    redirect_to settings_remote_accounts_url
   end
 end
