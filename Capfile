24a3c2fc496c618ee2bee561d43467b35048618a
Ichigo Mayo <IchigoMayo@mayodon.club>
2017-05-15 16:31:19 +0900

    capistrano: asset:precompile on local.

--- Capfile
+++ Capfile
@@ -8,7 +8,6 @@ install_plugin Capistrano::SCM::Git
 require 'capistrano/rbenv'
 require 'capistrano/bundler'
 require 'capistrano/yarn'
-require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
 
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -11,4 +11,21 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 set :migration_role, :app
 
 append :linked_files, '.env.production', 'public/robots.txt'
-append :linked_dirs, 'vendor/bundle', 'node_modules', 'public/system'
+append :linked_dirs, 'vendor/bundle', 'node_modules', 'public/system', 'public/packs'
+
+namespace :deploy do
+  after :updated, "assets:precompile"
+end
+
+namespace :assets do
+  task :precompile do
+    on roles(:web) do
+      rsync_host = host
+      run_locally do
+        execute "RAILS_ENV=#{fetch(:rails_env)} rake assets:precompile"
+        execute "rsync -av --delete public/assets/ #{rsync_host.user}@#{rsync_host.hostname}:#{shared_path}/public/assets/"
+        execute "rsync -av --delete public/packs/ #{rsync_host.user}@#{rsync_host.hostname}:#{shared_path}/public/packs/"
+      end
+    end
+  end
+end
